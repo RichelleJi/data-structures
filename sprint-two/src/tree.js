80dd63ab3236b92d0121cc5d70b74f5e3593f7cf
richelleji <ruixi.ji@gmail.com>
2018-06-14 18:08:52 -0700

    finished tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,32 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = {};
+  _.extend(child, Tree(value));
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  for (var elem in this.children) {
+    if (this.children[elem].value === target) {
+      return true;
+    }
+    for (var element in this.children[elem].children) {
+      if (this.children[elem].children[element].value === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
@@ -21,3 +34,6 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// addChild(value) is linear --> O(n)
+// contains(target) is linear --> O(n)
