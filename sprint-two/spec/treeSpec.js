1e570db77f41a43b67bd3124d4112a20f28f5170
richelleji <ruixi.ji@gmail.com>
2018-06-16 14:59:10 -0700

    added test for treeSpec

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -41,4 +41,15 @@ describe('tree', function() {
     expect(tree.contains(8)).to.equal(true);
   });
 
+  it('should correctly detect deeply nested children', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    tree.children[1].children[0].addChild(3);
+    tree.children[1].children[0].children[0].addChild(4);
+    expect(tree.contains(3)).to.equal(true);
+    expect(tree.contains(4)).to.equal(true);
+  });
+  
 });
